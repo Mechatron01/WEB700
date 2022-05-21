f5e50ce4ee54ae299a7942fa6e671df1e72b16af
MUTTAHEER <muttaheer24@gmail.com>
2022-05-21 17:33:44 -0400

    First commit

---
+++ test.js
@@ -0,0 +1,8 @@
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+  }
+
+x = getRandomInt(0,8)
+console.log(x)
